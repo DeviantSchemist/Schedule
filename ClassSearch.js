431b2dfc337451f60fda2909d08bb690acc12842
Michael Vartan <michael.vartan@student.csulb.edu>
2015-10-29 14:07:50 -0700

    converted comments to dockblocks, converted to a node module, and fixed a bug with section retrieval

--- ClassSearch.js
+++ ClassSearch.js
@@ -1,13 +1,24 @@
 // import requirements
-var cheerio = require('cheerio')
+var cheerio = require('cheerio');
 var Q = require('q');
 var http = require('follow-redirects').http;
 
 // extend Q prototype for map
+
+module.exports = {
+  httpRead: httpRead,
+  getSessions: getSessions,
+  getDepartmentsForSessionURL:getDepartmentsForSessionURL,
+  getCoursesForDepartmentURL:getCoursesForDepartmentURL
+}
+
+/**
+ * @param  {Array}      arr
+ * @param  {function}   iterator 
+ * @return {Promise}
+ */
 Q.map = function map (arr, iterator) {
-  // execute the func for each element in the array and collect the results
-  var promises = arr.map(function (el) { return iterator(el) })
-  return Q.all(promises) // return the group promise
+  return Q.all(arr.map(function (el) { return iterator(el) }));
 }
 
 // extend array prototype to remove undefined values
@@ -22,8 +33,12 @@ Array.prototype.clean = function() {
 };
 
 
-// promise for http request
-function httpRequest(url) {
+/**
+ * Generates a promise to retrieve the contents of a website
+ * @param  {String} url
+ * @return {Promise}
+ */
+function httpRead(url) {
   return Q.Promise(function(resolve, reject) {
     http.get(url, function(res) {
       var body = '';
@@ -42,9 +57,12 @@ function httpRequest(url) {
   })
 }
 
-// Promise to get a list of all sessions
+/**
+ * Generates a promise to retrieve all sessions from the CSULB website
+ * @return {Promise}
+ */
 function getSessions() {
-  return httpRequest("http://web.csulb.edu/depts/enrollment/registration/class_schedule/").then(function(value) {
+  return httpRead("http://web.csulb.edu/depts/enrollment/registration/class_schedule/").then(function(value) {
     return Q.Promise(function (resolve) {
       var $ = cheerio.load(value.toString());
       var terms = $(".term")
@@ -58,16 +76,20 @@ function getSessions() {
           "url"  : link.attr("href")
         };
       }).get();
-          // console.log("links:")
-      // console.log(links);
       resolve(links);
     })
   })
 }
 
-// Promise to get a list of all departments for a session
+
+/**
+ * Generates a promise to get a list of all departments for a session 
+ * from the CSULB website
+ * @param  {String}   sessionURL
+ * @return {Promise} 
+ */
 function getDepartmentsForSessionURL(sessionURL) {
-  return httpRequest(sessionURL).then(function(value) {
+  return httpRead(sessionURL).then(function(value) {
     var deferred = Q.defer();
     var $ = cheerio.load(value.toString());
     var urlPart = sessionURL.substring(0, sessionURL.lastIndexOf("/")+1)
@@ -84,14 +106,22 @@ function getDepartmentsForSessionURL(sessionURL) {
       };
     }).get());
     return deferred.promise;
-  },function(error) {
-    console.log("BAD"+error)
   });
 }
 
 // Promise to get a list of all courses in a department
+
+
+/**
+ * Promise to get a list of courses for a department from
+ * the CSULB website
+ *
+ * 
+ * @param  {String}   departmentURL
+ * @return {Promise}
+ */
 function getCoursesForDepartmentURL(departmentURL) {
-    return httpRequest(departmentURL).then(function(value) {
+    return httpRead(departmentURL).then(function(value) {
     var deferred = Q.defer();
     var $ = cheerio.load(value.toString());
     var courses = $(".courseBlock");
@@ -102,20 +132,23 @@ function getCoursesForDepartmentURL(departmentURL) {
       var sectionTable = $(courses[i]).find(".sectionTable tr");
       var sections = sectionTable.map(function(j) {
         if(j === 0) return undefined;
-        var sectionNumber = $(sectionTable[j]).find("td").eq(0).text()
-        var classNumber = $(sectionTable[j]).find("td").eq(1).text()
-        var type = $(sectionTable[j]).find("td").eq(3).text()
-        var days = $(sectionTable[j]).find("td").eq(4).text()
-        var time = $(sectionTable[j]).find("td").eq(5).text()
+        var sectionNumber = $(sectionTable[j]).find("th").eq(0).text()
+        var classNumber = $(sectionTable[j]).find("td").eq(0).text()
+        var type =  $(sectionTable[j]).find("td").eq(2).text()
+        var days = $(sectionTable[j]).find("td").eq(3).text()
+        var time = $(sectionTable[j]).find("td").eq(4).text().split("-")
+        var startTime = time[0];
+        var endTime = time[1];
         var open = $(sectionTable[j]).find(".dot").length > 0;
-        var location = $(sectionTable[j]).find("td").eq(7).text()
-        var instructor = $(sectionTable[j]).find("td").eq(8).text()
+        var location = $(sectionTable[j]).find("td").eq(6).text()
+        var instructor = $(sectionTable[j]).find("td").eq(7).text()
         return {
           "sectionNumber" : sectionNumber,
           "classNumber" : classNumber,
           "type" : type,
           "days" : days,
-          "time" : time,
+          "startTime" : startTime, //TODO: Convert this to a 24 hour date instead of string
+          "endTime" : endTime, // with no concept of am/pm in the start time :(
           "open" : open,
           "location" : location,
           "instructor" : instructor
@@ -130,21 +163,19 @@ function getCoursesForDepartmentURL(departmentURL) {
 
     }).get());
     return deferred.promise;
-  },function(error) {
-    console.log("BAD"+error)
   });
 }
 
-
-// execute code for testing.
-getSessions()
-  .then(function(sessions) {
-    return getDepartmentsForSessionURL(sessions[0].url)
-  })
-  .then(function(departments) {
-    return getCoursesForDepartmentURL(departments[0].url)
-  })
-  .then(function(courses) {
-    console.log(courses[0])
-  })
-
+if(!module.parent) {
+  // execute code for testing.
+  getSessions()
+    .then(function(sessions) {
+      return getDepartmentsForSessionURL(sessions[0].url)
+    })
+    .then(function(departments) {
+      return getCoursesForDepartmentURL(departments[0].url)
+    })
+    .then(function(courses) {
+      console.log(courses[0])
+    })
+}
